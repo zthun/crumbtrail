656843c68469082bc1abcfe2d7e5978bc67d0efc
Anthony Bonta <zthun.coding@gmail.com>
2025-10-15 12:58:27 -0500

    feat: resolve path like converts a PathLike variable to a string

--- packages/crumbtrail-fs/src/index.mts
+++ packages/crumbtrail-fs/src/index.mts
@@ -1,3 +1,4 @@
 export * from "./file-system/file-system-node.mjs";
 export * from "./file-system/file-system-repository.mjs";
 export * from "./file-system/file-system-service.mjs";
+export * from "./resolve-path-like/resolve-path-like.js";
---
+++ packages/crumbtrail-fs/src/resolve-path-like/resolve-path-like.spec.ts
@@ -0,0 +1,44 @@
+import { join, resolve } from "node:path";
+import { pathToFileURL } from "node:url";
+import { describe, expect, it } from "vitest";
+import { resolvePathLike } from "./resolve-path-like.js";
+
+describe("Path Like to String", () => {
+  it("returns the resolution of the string when the path is already a string", () => {
+    // Arrange.
+    const target = join("tmp", "test.txt");
+    const expected = resolve(target);
+
+    // Act.
+    const actual = resolvePathLike(target);
+
+    // Assert.
+    expect(actual).toEqual(expected);
+  });
+
+  it("converts a buffer path into a string", () => {
+    // Arrange.
+    const target = join("tmp", "buffer.txt");
+    const buffer = Buffer.from(target);
+    const expected = resolve(target);
+
+    // Act.
+    const actual = resolvePathLike(buffer);
+
+    // Assert.
+    expect(actual).toEqual(expected);
+  });
+
+  it("converts a file URL into a native file system path", () => {
+    // Arrange.
+    const target = join("tmp", "url.txt");
+    const url = pathToFileURL(target);
+    const expected = resolve(".", target);
+
+    // Act.
+    const actual = resolvePathLike(url);
+
+    // Assert.
+    expect(actual).toEqual(expected);
+  });
+});
